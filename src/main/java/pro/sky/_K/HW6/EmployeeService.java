1d3604b7d8e0dff1ddfdb787998957545f4c0241
pojar <pojarka@mail.com>
2024-10-14 13:00:59 +0500

    Changes for homework by Map

--- src/main/java/pro/sky/_K/HW6/EmployeeService.java
+++ src/main/java/pro/sky/_K/HW6/EmployeeService.java
@@ -2,57 +2,62 @@ package pro.sky._K.HW6;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService implements EmpoyeeService1 {
     private static final int MAX_EMPLOYEES = 100;
-    private final List<Employee> employees;
+    private final Map<String, Employee> employees;
+    private java.util.Collection<Employee> Collection;
 
     public EmployeeService() {
-        this.employees = new ArrayList<>();
+        this.employees = new HashMap<>(MAX_EMPLOYEES);
     }
 
+
     @Override
-    public Employee addEmployee(String firstName, String lastName)  {
+    public Employee addEmployee(String firstName, String lastName) {
         if (employees.size() >= MAX_EMPLOYEES) {
             throw new MaxEmployeesReachedException("Максимальное количество сотрудников уже достигнуто");
         }
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(employee)) {
             throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть");
         }
-        employees.add(employee);
+        var key = makekey(firstName, lastName);
+        employees.put(key, employee);
         return employee;
     }
 
-    @Override
-    public Employee removeEmployee(String firstName, String lastName) throws EmployeeNotFoundException {
-        Employee employeeToRemove = findEmployee(firstName, lastName);
-        if (employeeToRemove != null) {
-            employees.remove(employeeToRemove);
-        } else {
-            throw new EmployeeNotFoundException("Сотрудник не найден");
-        }
-        return employeeToRemove;
+    private static String makekey(String firstName, String lastName) {
+        return (firstName + "_" + lastName).toLowerCase();
     }
 
     @Override
+    public Employee removeEmployee(String firstName, String lastName) {
+        var key = makekey(firstName, lastName);
+        var removed = employees.remove(key);
+        if (removed == null) {
+            throw new EmployeeNotFoundException("Сотрудник не найден");
+        }
+        return removed;
+    }
+
+
+    @Override
     public Employee findEmployee(String firstName, String lastName) {
-        for (Employee employee : employees) {
-            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
-                return employee;
-            }
+        var key = makekey(firstName, lastName);
+        if (employees.containsKey(key)) {
+            return employees.get(key);
         }
         throw new EmployeeNotFoundException("Сотрудник не найден");
     }
 
+
     @Override
     public Collection<Employee> getAll() {
-        return List.of();
+        return Collection;
     }
-
 }
+
 
